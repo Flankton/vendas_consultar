23b453d38a81762b7c9326d05ade960a7c07af0e
flankton <will.engecomp@gmail.com>
2022-06-05 13:52:51 -0300

    first commit

---
+++ app/Http/Controllers/VendasController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class VendasController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+
+    public function vendas(){
+
+        return 'hello world';
+    }
+}
